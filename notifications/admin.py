1e1707e1ab61eaf69fa6b82b684b8a01b49d8794
suvas <ss5138275@gmail.com>
2026-05-27 02:12:05 +0545

    notification

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'is_read', 'created_at']
+    list_filter = ['is_read', 'created_at']
+    search_fields = ['user__email', 'title', 'body']
+    readonly_fields = ['created_at']
+    ordering = ['-created_at']
